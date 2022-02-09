025f30c22142fdddac179a12ea0361a922c9cbd5
aeon <jerry73204@gmail>
2022-02-10 03:15:21 +0800

    Add PacketKind type

--- velodyne-lidar/src/packet.rs
+++ velodyne-lidar/src/packet.rs
@@ -11,6 +11,73 @@ use crate::{
 };
 use std::f64::consts::PI;
 
+pub use kind::*;
+mod kind {
+    use super::*;
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub enum PacketKind {
+        Data(Box<DataPacket>),
+        Position(Box<PositionPacket>),
+    }
+
+    impl PacketKind {
+        pub fn from_slice(buffer: &[u8]) -> Result<Self> {
+            Ok(if let Ok(packet) = DataPacket::from_slice(buffer) {
+                (*packet).into()
+            } else if let Ok(packet) = PositionPacket::from_slice(buffer) {
+                (*packet).into()
+            } else {
+                bail!("unable to parse bytes into a data or a positoin packet");
+            })
+        }
+
+        pub fn try_into_data(self) -> Result<DataPacket, Self> {
+            if let Self::Data(v) = self {
+                Ok(*v)
+            } else {
+                Err(self)
+            }
+        }
+
+        pub fn try_into_position(self) -> Result<PositionPacket, Self> {
+            if let Self::Position(v) = self {
+                Ok(*v)
+            } else {
+                Err(self)
+            }
+        }
+
+        pub fn as_data(&self) -> Option<&DataPacket> {
+            if let Self::Data(v) = self {
+                Some(v)
+            } else {
+                None
+            }
+        }
+
+        pub fn as_position(&self) -> Option<&PositionPacket> {
+            if let Self::Position(v) = self {
+                Some(v)
+            } else {
+                None
+            }
+        }
+    }
+
+    impl From<PositionPacket> for PacketKind {
+        fn from(v: PositionPacket) -> Self {
+            Self::Position(Box::new(v))
+        }
+    }
+
+    impl From<DataPacket> for PacketKind {
+        fn from(v: DataPacket) -> Self {
+            Self::Data(Box::new(v))
+        }
+    }
+}
+
 pub use data_packet::*;
 mod data_packet {
 
