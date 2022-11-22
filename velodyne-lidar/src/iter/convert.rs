c3b6752b5185bcaccbaec4bfcd76da1e6857d285
aeon <jerry73204@gmail.com>
2022-11-22 18:21:45 +0800

    Add try_packet_to_frame_xyz*() methods

--- velodyne-lidar/src/iter/convert.rs
+++ velodyne-lidar/src/iter/convert.rs
@@ -1,16 +1,7 @@
 //! Iterator conversion functions.
 
-use crate::{
-    batcher::Batcher,
-    firing_xyz::{FiringXyzD16, FiringXyzD32, FiringXyzS16, FiringXyzS32},
-    frame_xyz::{FrameXyz, FrameXyzD16, FrameXyzD32, FrameXyzS16, FrameXyzS32},
-    kinds::{Format, FormatKind},
-    Config, Config16, Config32, DataPacket,
-};
-use anyhow::{anyhow, Result};
+use crate::kinds::Format;
 use log::warn;
-
-pub(crate) type FrameXyzIter<'a> = Box<dyn Iterator<Item = FrameXyz> + Send + 'a>;
 
 fn audit_format(packet_format: Option<Format>, config_format: Format) {
     match packet_format {
@@ -29,85 +20,235 @@ fn audit_format(packet_format: Option<Format>, config_format: Format) {
     }
 }
 
-/// Converts an iterator of packets to an iterator of [FrameXyz].
-pub fn packet_to_frame_xyz<'a, I>(config: Config, packets: I) -> Result<FrameXyzIter<'a>>
-where
-    I: IntoIterator<Item = DataPacket> + 'a,
-    I::IntoIter: Send,
-{
-    use FormatKind as K;
-
-    let config_kinds = config
-        .try_into_kind()
-        .map_err(|_| anyhow!("invalid configuration"))?;
-
-    let iter: FrameXyzIter = match config_kinds {
-        K::Single16(config) => Box::new(packet_to_frame_xyz_s16(config, packets).map(K::from_s16)),
-        K::Single32(config) => Box::new(packet_to_frame_xyz_s32(config, packets).map(K::from_s32)),
-        K::Dual16(config) => Box::new(packet_to_frame_xyz_d16(config, packets).map(K::from_d16)),
-        K::Dual32(config) => Box::new(packet_to_frame_xyz_d32(config, packets).map(K::from_d32)),
+pub use data_packet_to_frame_xyz::*;
+mod data_packet_to_frame_xyz {
+    use super::audit_format;
+    use crate::{
+        batcher::Batcher,
+        firing_xyz::{FiringXyzD16, FiringXyzD32, FiringXyzS16, FiringXyzS32},
+        frame_xyz::{FrameXyz, FrameXyzD16, FrameXyzD32, FrameXyzS16, FrameXyzS32},
+        kinds::FormatKind,
+        Config, Config16, Config32, DataPacket,
     };
+    use anyhow::{anyhow, Result};
 
-    Ok(iter)
+    pub(crate) type FrameXyzIter<'a> = Box<dyn Iterator<Item = FrameXyz> + Send + 'a>;
+
+    /// Converts an iterator of packets to an iterator of [FrameXyz].
+    pub fn data_packet_to_frame_xyz<'a, I>(config: Config, packets: I) -> Result<FrameXyzIter<'a>>
+    where
+        I: IntoIterator<Item = DataPacket> + 'a,
+        I::IntoIter: Send,
+    {
+        use FormatKind as K;
+
+        let config_kinds = config
+            .try_into_kind()
+            .map_err(|_| anyhow!("invalid configuration"))?;
+
+        let iter: FrameXyzIter = match config_kinds {
+            K::Single16(config) => {
+                Box::new(data_packet_to_frame_xyz_s16(config, packets).map(K::from_s16))
+            }
+            K::Single32(config) => {
+                Box::new(data_packet_to_frame_xyz_s32(config, packets).map(K::from_s32))
+            }
+            K::Dual16(config) => {
+                Box::new(data_packet_to_frame_xyz_d16(config, packets).map(K::from_d16))
+            }
+            K::Dual32(config) => {
+                Box::new(data_packet_to_frame_xyz_d32(config, packets).map(K::from_d32))
+            }
+        };
+
+        Ok(iter)
+    }
+
+    macro_rules! declare_data_packet_to_frame_xyz_fn {
+        ($name:ident, $config:ident, $firing:ident, $frame:ident, $iter_fn:ident) => {
+            pub fn $name<I>(config: $config, packets: I) -> impl Iterator<Item = $frame> + Send
+            where
+                I: IntoIterator<Item = DataPacket>,
+                I::IntoIter: Send,
+            {
+                let batcher: Batcher<$firing> = Batcher::new();
+
+                packets
+                    .into_iter()
+                    .map(move |packet| {
+                        audit_format(packet.try_format(), config.format());
+
+                        let firings: Vec<$firing> = packet
+                            .$iter_fn()
+                            .map(|block| block.to_firing_xyz(&config))
+                            .collect();
+                        firings
+                    })
+                    .scan(batcher, |batcher, firings| {
+                        let frames: Vec<_> = batcher
+                            .push_many(firings)
+                            .map(|firings| $frame { firings })
+                            .collect();
+                        Some(frames)
+                    })
+                    .flatten()
+            }
+        };
+    }
+
+    declare_data_packet_to_frame_xyz_fn!(
+        data_packet_to_frame_xyz_s16,
+        Config16,
+        FiringXyzS16,
+        FrameXyzS16,
+        firing_block_iter_s16
+    );
+    declare_data_packet_to_frame_xyz_fn!(
+        data_packet_to_frame_xyz_s32,
+        Config32,
+        FiringXyzS32,
+        FrameXyzS32,
+        firing_block_iter_s32
+    );
+    declare_data_packet_to_frame_xyz_fn!(
+        data_packet_to_frame_xyz_d16,
+        Config16,
+        FiringXyzD16,
+        FrameXyzD16,
+        firing_block_iter_d16
+    );
+    declare_data_packet_to_frame_xyz_fn!(
+        data_packet_to_frame_xyz_d32,
+        Config32,
+        FiringXyzD32,
+        FrameXyzD32,
+        firing_block_iter_d32
+    );
 }
 
-macro_rules! declare_packet_to_frame_xyz_fn {
-    ($name:ident, $config:ident, $firing:ident, $frame:ident, $iter_fn:ident) => {
-        pub fn $name<I>(config: $config, packets: I) -> impl Iterator<Item = $frame> + Send
-        where
-            I: IntoIterator<Item = DataPacket>,
-            I::IntoIter: Send,
-        {
-            let batcher: Batcher<$firing> = Batcher::new();
-
-            packets
-                .into_iter()
-                .map(move |packet| {
-                    audit_format(packet.try_format(), config.format());
-
-                    let firings: Vec<$firing> = packet
-                        .$iter_fn()
-                        .map(|block| block.to_firing_xyz(&config))
-                        .collect();
-                    firings
-                })
-                .scan(batcher, |batcher, firings| {
-                    let frames: Vec<_> = batcher
-                        .push_many(firings)
-                        .map(|firings| $frame { firings })
-                        .collect();
-                    Some(frames)
-                })
-                .flatten()
-        }
+pub use try_packet_to_frame_xyz::*;
+mod try_packet_to_frame_xyz {
+    use super::audit_format;
+    use crate::{
+        batcher::Batcher,
+        firing_xyz::{FiringXyzD16, FiringXyzD32, FiringXyzS16, FiringXyzS32},
+        frame_xyz::{FrameXyz, FrameXyzD16, FrameXyzD32, FrameXyzS16, FrameXyzS32},
+        kinds::FormatKind,
+        Config, Config16, Config32, Packet,
     };
-}
+    use anyhow::{anyhow, Result};
+    use itertools::Itertools;
 
-declare_packet_to_frame_xyz_fn!(
-    packet_to_frame_xyz_s16,
-    Config16,
-    FiringXyzS16,
-    FrameXyzS16,
-    firing_block_iter_s16
-);
-declare_packet_to_frame_xyz_fn!(
-    packet_to_frame_xyz_s32,
-    Config32,
-    FiringXyzS32,
-    FrameXyzS32,
-    firing_block_iter_s32
-);
-declare_packet_to_frame_xyz_fn!(
-    packet_to_frame_xyz_d16,
-    Config16,
-    FiringXyzD16,
-    FrameXyzD16,
-    firing_block_iter_d16
-);
-declare_packet_to_frame_xyz_fn!(
-    packet_to_frame_xyz_d32,
-    Config32,
-    FiringXyzD32,
-    FrameXyzD32,
-    firing_block_iter_d32
-);
+    pub(crate) type FrameXyzIter<'a, E> = Box<dyn Iterator<Item = Result<FrameXyz, E>> + Send + 'a>;
+
+    /// Converts an iterator of packets to an iterator of [FrameXyz].
+    pub fn try_packet_to_frame_xyz<'a, E, I>(
+        config: Config,
+        packets: I,
+    ) -> Result<FrameXyzIter<'a, E>>
+    where
+        I: IntoIterator<Item = Result<Packet, E>> + 'a,
+        I::IntoIter: Send,
+        E: 'a,
+    {
+        use FormatKind as K;
+
+        let config_kinds = config
+            .try_into_kind()
+            .map_err(|_| anyhow!("invalid configuration"))?;
+
+        let iter: FrameXyzIter<_> = match config_kinds {
+            K::Single16(config) => {
+                Box::new(try_packet_to_frame_xyz_s16(config, packets).map_ok(K::from_s16))
+            }
+            K::Single32(config) => {
+                Box::new(try_packet_to_frame_xyz_s32(config, packets).map_ok(K::from_s32))
+            }
+            K::Dual16(config) => {
+                Box::new(try_packet_to_frame_xyz_d16(config, packets).map_ok(K::from_d16))
+            }
+            K::Dual32(config) => {
+                Box::new(try_packet_to_frame_xyz_d32(config, packets).map_ok(K::from_d32))
+            }
+        };
+
+        Ok(iter)
+    }
+
+    macro_rules! declare_try_packet_to_frame_xyz_fn {
+        ($name:ident, $config:ident, $firing:ident, $frame:ident, $iter_fn:ident) => {
+            pub fn $name<E, I>(
+                config: $config,
+                packets: I,
+            ) -> impl Iterator<Item = Result<$frame, E>> + Send
+            where
+                I: IntoIterator<Item = Result<Packet, E>>,
+                I::IntoIter: Send,
+            {
+                let batcher: Batcher<$firing> = Batcher::new();
+
+                packets
+                    .into_iter()
+                    .map_ok(|packet| packet.try_into_data().ok())
+                    .flatten_ok()
+                    .map_ok(move |packet| {
+                        audit_format(packet.try_format(), config.format());
+
+                        let firings: Vec<$firing> = packet
+                            .$iter_fn()
+                            .map(|block| block.to_firing_xyz(&config))
+                            .collect();
+                        firings
+                    })
+                    .scan(Some(batcher), |batcher, firings| {
+                        let firings = match firings {
+                            Ok(firings) => firings,
+                            Err(err) => {
+                                *batcher = None;
+                                return Some(Err(err));
+                            }
+                        };
+                        let Some(batcher) = batcher else {
+                                                                                        return None;
+                                                                                    };
+
+                        let frames: Vec<_> = batcher
+                            .push_many(firings)
+                            .map(|firings| $frame { firings })
+                            .collect();
+                        Some(Ok(frames))
+                    })
+                    .flatten_ok()
+            }
+        };
+    }
+
+    declare_try_packet_to_frame_xyz_fn!(
+        try_packet_to_frame_xyz_s16,
+        Config16,
+        FiringXyzS16,
+        FrameXyzS16,
+        firing_block_iter_s16
+    );
+    declare_try_packet_to_frame_xyz_fn!(
+        try_packet_to_frame_xyz_s32,
+        Config32,
+        FiringXyzS32,
+        FrameXyzS32,
+        firing_block_iter_s32
+    );
+    declare_try_packet_to_frame_xyz_fn!(
+        try_packet_to_frame_xyz_d16,
+        Config16,
+        FiringXyzD16,
+        FrameXyzD16,
+        firing_block_iter_d16
+    );
+    declare_try_packet_to_frame_xyz_fn!(
+        try_packet_to_frame_xyz_d32,
+        Config32,
+        FiringXyzD32,
+        FrameXyzD32,
+        firing_block_iter_d32
+    );
+}
